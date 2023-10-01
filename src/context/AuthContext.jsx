afbf4a7461334051dfc3942560bd56b179ec6040
VaNomad <sjur.hassel@gmail.com>
2023-10-01 16:13:48 +0100

    Adds  basic context component setup

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { useState, useContext, createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+  <AuthContext.Provider value={ { user, setUser } }>
+    {children}
+  </AuthContext.Provider>
+  )
+}
+
+export const UseAuth = () => {
+  return useContext(AuthContext);
+}
+
