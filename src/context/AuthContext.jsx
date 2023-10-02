0b116757ca9aecd0567f4d7d31a6f201fcafcf44
VaNomad <sjur.hassel@gmail.com>
2023-10-02 15:19:31 +0100

    adds local storage to context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,16 +4,18 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("authToken"));
 
   const login = (userData, authToken) => {
     setUser(userData);
     setToken(authToken);
+    localStorage.setItem("authToken", authToken)
   }
 
   const logout = () => {
     setUser(null);
     setToken(null);
+    localStorage.removeItem("authToken")
   }
 
   const isAuthenticated = () => {
