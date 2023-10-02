1d9c33a61398de22452cf38169e3333f65f30b38
VaNomad <sjur.hassel@gmail.com>
2023-10-02 15:10:05 +0100

    updates and adds auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,9 +4,24 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const login = (userData, authToken) => {
+    setUser(userData);
+    setToken(authToken);
+  }
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+  }
+
+  const isAuthenticated = () => {
+    return !!token;
+  }
 
   return (
-  <AuthContext.Provider value={ { user, setUser } }>
+  <AuthContext.Provider value={ { user, token, login, logout, isAuthenticated } }>
     {children}
   </AuthContext.Provider>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
