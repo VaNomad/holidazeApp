5ee2712a38f4cc2e828a20f7ba2134d2a980ceb7
VaNomad <sjur.hassel@gmail.com>
2023-10-04 15:35:36 +0100

    imports storage pickup file and adds to profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,18 @@
-// import { Link } from "react-router-dom";
+import { CurrentStorage } from "../../utils/CurrentStorage";
 import header from "../../assets/images/maldives1.jpg"
-import dummyProfile from "../../assets/vectors/hLogoGreen.png"
+// import dummyProfile from "../../assets/vectors/hLogoGreen.png"
 import { useState } from "react";
 import { AiOutlineUser, AiOutlineMail, AiOutlineCamera, AiOutlineEye } from "react-icons/ai";
+import { BsKey } from "react-icons/bs"
+import {GiCheckMark, GiCrossMark} from "react-icons/gi"
+import noAvatar from "../../assets/vectors/hLogoGreen.png"
 import { ChangeAvatarModal } from "../../components/ui/modals/ChangeAvatarModal";
 
 export const Profile = () => {
   const [isModalOpen, setModalOpen] = useState(false)
   const openModal = () => setModalOpen(true)
   const closeModal = () => setModalOpen(false)
+  const userData = CurrentStorage();
 
   return (
     <div className="bg-black">
@@ -22,7 +26,17 @@ export const Profile = () => {
         </div>
         <div className="absolute top-[25%] md:top-[18%] right-[25%] md:right-[30%] flex flex-col">
           <div className=" h-[150px] md:h-[200px] w-[150px] md:w-[200px] border-4 border-zinc-700 bg-black rounded-full px-2">
-            <img src={dummyProfile} alt="" className="" />
+            <img
+              src={
+                userData.avatar ? (
+                  <img src={userData.avatar} />
+                ) : (
+                  <img src={noAvatar} />
+                )
+              }
+              alt=""
+              className="w-full h-full"
+            />
           </div>
           <button
             onClick={openModal}
@@ -37,14 +51,14 @@ export const Profile = () => {
         <h1 className="font-alli text-5xl md:text-[5rem]">Profile</h1>
       </div>
       <div className="flex flex-col items-center justify-evenly h-[45vh]">
-        <div className="relative ps-24 pe-20 py-2 border-[2px] border-holipink rounded-full w-[60%] md:max-w-[30rem]">
+        <div className="relative ps-24 pe-20 py-2 border-[2px] border-holipink rounded-full w-[60%] h- md:max-w-[30rem]">
           <div className="absolute top-0 left-0">
             <AiOutlineUser
               size={40}
               className="text-black bg-holipink rounded-full p-1"
             />
           </div>
-          <p className="whitespace-nowrap">Users Name</p>
+          <p className="whitespace-nowrap">{userData.username}</p>
         </div>
         <div className="relative ps-24 pe-20 py-2 border-[2px] border-holipink rounded-full w-[60%] md:max-w-[30rem]">
           <div className="absolute top-0 left-0">
@@ -53,7 +67,7 @@ export const Profile = () => {
               className="text-black bg-holipink rounded-full p-1"
             />
           </div>
-          <p className="whitespace-nowrap">Email</p>
+          <p className="whitespace-nowrap">{userData.email}</p>
         </div>
         <div className="relative ps-24 pe-20 py-2 border-[2px] border-holipink rounded-full w-[60%] md:max-w-[30rem]">
           <div className="absolute top-0 left-0">
@@ -62,16 +76,23 @@ export const Profile = () => {
               className="text-black bg-holipink rounded-full p-1"
             />
           </div>
-          <p className="whitespace-nowrap">Avatar</p>
+          <p className="whitespace-nowrap overflow-hidden">{userData.avatar}</p>
         </div>
         <div className="relative ps-24 pe-20 py-2 border-[2px] border-holipink rounded-full w-[60%] md:max-w-[30rem]">
           <div className="absolute top-0 left-0">
-            <AiOutlineEye
+            <BsKey
               size={40}
               className="text-black bg-holipink rounded-full p-1"
             />
           </div>
-          <p className="whitespace-nowrap">Password</p>
+          <p className="whitespace-nowrap flex items-center justify-between">
+            Venue Manager:{" "}
+            {userData.manager ? (
+              <GiCheckMark color="#70C376" />
+            ) : (
+              <GiCrossMark color="#C37070" size={20} />
+            )}
+          </p>
         </div>
       </div>
     </div>
