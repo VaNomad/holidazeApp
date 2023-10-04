f619c7a8992f41906297642180201a1ce2efb4be
VaNomad <sjur.hassel@gmail.com>
2023-10-05 00:07:54 +0100

    Adds api call for updating the avatar img

---
+++ src/api/UpdateAvatarUrl.jsx
@@ -0,0 +1,31 @@
+import { API_BASE_URL } from "./endpoints";
+
+export async function UpdateAvatarUrl(userName, newAvatarUrl) {
+  const accessToken = localStorage.getItem("accessToken");
+
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/profiles/${userName}/media`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ avatar: newAvatarUrl }),
+      }
+    );
+
+    if (!response.ok) {
+      console.error("updateAvatarUrl Response: ", response);
+      throw new Error("Could not update avatar URL");
+    }
+
+    const avatarData = await response.json();
+
+    return avatarData;
+  } catch (error) {
+    console.error("Updating Avatar failed: ", error);
+    throw new Error("Updating Avatar failed");
+  }
+}
