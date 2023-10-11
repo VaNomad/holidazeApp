dd5da25485aafe1786e658e7bb61a53130ef9b15
VaNomad <sjur.hassel@gmail.com>
2023-10-11 11:56:11 +0100

    style error update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -50,7 +50,7 @@ export const Profile = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-evenly h-[40vh]">
-        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] h- md:max-w-[30rem]">
+        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] sm:max-w-[30rem]">
           <div className="absolute top-0 left-[-1px]">
             <AiOutlineUser
               size={32}
@@ -59,7 +59,7 @@ export const Profile = () => {
           </div>
           {user && <p>{user.name}</p>}
         </div>
-        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] md:max-w-[30rem]">
+        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] sm:max-w-[30rem]">
           <div className="absolute top-0 left-0">
             <AiOutlineMail
               size={32}
@@ -68,7 +68,7 @@ export const Profile = () => {
           </div>
           <p className="whitespace-nowrap">{user.email}</p>
         </div>
-        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] md:max-w-[30rem]">
+        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] sm:max-w-[30rem]">
           <div className="absolute top-0 left-0">
             <AiOutlineCamera
               size={32}
@@ -77,7 +77,7 @@ export const Profile = () => {
           </div>
           <p className="whitespace-nowrap overflow-hidden">{user.avatar}</p>
         </div>
-        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] md:max-w-[30rem]">
+        <div className="relative ps-14 pe-5 py-1 border-[2px] border-holipink rounded-full w-[80%] sm:max-w-[30rem]">
           <div className="absolute top-0 left-0">
             <BsKey
               size={32}
