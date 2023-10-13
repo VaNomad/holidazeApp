76a4fd5b6c2761245c90255ad12708fbcf021e5d
VaNomad <sjur.hassel@gmail.com>
2023-10-13 22:19:11 +0100

    Adds storage var for response in card

--- src/components/cards/BookingsCard.jsx
+++ src/components/cards/BookingsCard.jsx
@@ -3,6 +3,20 @@
 import { BtnFull } from "../ui/buttons/BtnFull";
 
 export const BookingsCard = ({ data }) => {
+
+  const {
+    created: venueCreated,
+    description,
+    id: venueId,
+    location: { address, city, continent, country, zip },
+    maxGuests,
+    media,
+    meta: { breakfast, parking, pets, wifi },
+    name,
+    price,
+    rating,
+  } = data.venue;
+
   return (
     <div className="rounded-2xl p-2">
       <div className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -10,32 +24,48 @@ export const BookingsCard = ({ data }) => {
           <div className="card container mx-auto p-3 ">
             <div>
               <img
-                src={data.avatar}
+                src={media}
                 alt="User Avatar"
                 className="w-[20px] h-[20px] rounded-full object-cover"
               />
             </div>
-            <h2>{data.name}</h2>
-            <p>{data.email}</p>
-            {data.bookings && (
+            <h2>{name}</h2>
+            <p>{venueId}</p>
+            <div>
               <div>
+                <p>Max number of guests: {maxGuests}</p>
                 <div>
-                  <p>Guests: {data.booking[0].guests}</p>
-                  <div>
-                    <img
-                      src={data.booking[0].venue.media[0]}
-                      alt=""
-                      className="w-[20px] h-[20px] rounded-full object-cover"
-                    />
-                  </div>
-                  <h3>Location Data:</h3>
-                  <p>Address: {data.bookings[0].venue.location.address}</p>
-                  <p>City: {data.bookings[0].venue.location.city}</p>
-                  <p>Zip: {data.bookings[0].venue.location.zip}</p>
+                  <img
+                    src={media}
+                    alt=""
+                    className="w-[20px] h-[20px] rounded-full object-cover"
+                  />
                 </div>
-                <BtnFull />
+                <div>
+                  <h5>Description:</h5>
+                  <p>{description}</p>
+                </div>
+                <div>
+                  <h3>Location Data:</h3>
+                  <p>Venue Created: {venueCreated}</p>
+                  <p>Continent: {continent}</p>
+                  <p>Country: {country}</p>
+                  <p>Address: {address}</p>
+                  <p>City: {city}</p>
+                  <p>Zip: {zip}</p>
+                </div>
+                <div>
+                  <h3>Services:</h3>
+                  <p>Breakfast: {breakfast}</p>
+                  <p>Parking: {parking}</p>
+                  <p>Pets allowed: {pets}</p>
+                  <p>Wifi: { wifi }</p>
+                  <p>Price pr night: { price }</p>
+                  <p>Rating {rating} </p>
+                </div>
               </div>
-            )}
+              <BtnFull />
+            </div>
           </div>
         ) : (
           <p>Loading...</p>
