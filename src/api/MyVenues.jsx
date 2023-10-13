a37720385de60edcb73f184c42f1502c492d9870
VaNomad <sjur.hassel@gmail.com>
2023-10-13 19:09:16 +0100

    Adds MyVenues api call

---
+++ src/api/MyVenues.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "./endpoints";
+import { ErrorDisplay } from "../components/ui/messages/ErrorDisplay";
+import { Loader } from "../components/ui/loader/Loader";
+import { MyVenuesCard } from "../components/cards/MyVenuesCard";
+
+export function MyVenues() {
+  const [venueData, setVenueData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [errorDisplay, setErrorDisplay] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+      console.log(accessToken);
+      const user = localStorage.getItem("username");
+      console.log(user);
+      const isUrl = `${API_BASE_URL}/profiles/${user}/venues?_bookings=true&_venues=true;`;
+
+      try {
+        setIsLoading(true);
+        setHasError(false);
+        const response = await fetch(isUrl, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(
+            `Could not return data from the API: Status: ${response.status}`
+          );
+        }
+
+        console.log(response);
+
+        const data = await response.json();
+        console.log(data);
+        // console.log("bookingData:", bookingData);
+
+        setVenueData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error:", error);
+        setHasError(true);
+        setErrorDisplay(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading || !venueData) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <div>
+        <ErrorDisplay message={errorDisplay} />
+      </div>
+    );
+  }
+
+  console.log("LAST LOG BEFORE MOUNT:", venueData);
+  return (
+    <div>
+      {venueData.bookings.map((data) => {
+        console.log(data);
+        return <MyVenuesCard data={data} key={data.id} />;
+      })}
+    </div>
+  );
+}
