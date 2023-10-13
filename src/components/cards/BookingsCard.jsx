23ec42e64f1991fcccf71703e5a1ffd26c9790be
VaNomad <sjur.hassel@gmail.com>
2023-10-13 01:38:09 +0100

    Adds bookingsCard

---
+++ src/components/cards/BookingsCard.jsx
@@ -0,0 +1,67 @@
+// import { MyBookings } from "../../api/MyBookings";
+import { VenuesCarousel } from "../carousel/VenuesCarousel";
+import { BtnFull } from "../ui/buttons/BtnFull";
+
+export const BookingsCard = ({ data }) => {
+  console.log(data);
+  const {
+    dateFrom,
+    dateTo,
+    guests,
+    venue: { name, media, price, location },
+  } = data;
+  console.log("Location:", location);
+
+  return (
+    <div className="rounded-2xl p-5 mt-12">
+      <div className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        <h1 className="font-dm font-thin text-[20px] uppercase flex flex-wrap p-1">
+          {name}
+        </h1>
+      </div>
+      <div>
+        <img
+          src={media[0]}
+          alt="venue image"
+          className="object-cover border-2 mx-auto rounded-2xl h-52"
+        />
+      </div>
+      <VenuesCarousel media={media} name={name} />
+
+      <div className="text-xl mt-1 p-2 bg-zinc-800 rounded-xl">
+        <div>
+          <h3>Your Stay</h3>
+        </div>
+        <div>
+          <div>
+            <h5>From</h5>
+            <p>{dateFrom}</p>
+          </div>
+          <div>
+            <h5>To</h5>
+            <p>{dateTo}</p>
+          </div>
+        </div>
+        <div>
+          <h5>Guests</h5>
+          <p>{guests}</p>
+        </div>
+        <div>
+          <h5>Location</h5>
+          <div>
+            <p>{location.country}</p>
+            <p>{location.city}</p>
+            <p>{location.address}</p>
+          </div>
+        </div>
+        <div>
+          <h5>Price</h5>
+          <p>{price}</p>
+        </div>
+        <div>
+          <BtnFull size={10} />
+        </div>
+      </div>
+    </div>
+  );
+};
