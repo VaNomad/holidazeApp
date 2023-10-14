a3197adeaf1097cede9995b57176b45a6c6498c6
VaNomad <sjur.hassel@gmail.com>
2023-10-14 22:53:10 +0100

    Adds BookingCall

---
+++ src/api/BookingCall.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const BookingCall = (url) => {
+  const [createBookingData, setCreateBookingData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const postData = async (createData) => {
+    const accessToken = localStorage.getItem("accessToken");
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(createData),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Error sending data", response);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setCreateBookingData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createBookingData, loading, error, postData };
+};
