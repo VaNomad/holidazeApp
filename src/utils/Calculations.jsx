6fee55fada75ed48b9a248c1b62cee870c25edb6
VaNomad <sjur.hassel@gmail.com>
2023-10-15 10:36:33 +0100

    updates calculations functions

--- src/utils/Calculations.jsx
+++ src/utils/Calculations.jsx
@@ -1,7 +1,16 @@
 export const calculateTotalPrice = (startDate, endDate, pricePerNight) => {
-  const numberOfNights = Math.ceil(
-    (endDate - startDate) / (1000 * 60 * 60 * 24)
-  );
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const timeDiff = end - start;
+  const numberOfNights = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
   const totalPrice = numberOfNights * pricePerNight;
   return totalPrice;
-}
+};
+
+export const calculateTotalDays = (startDate, endDate) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const timeDiff = end - start;
+  const totalDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  return totalDays;
+};
