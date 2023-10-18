422da428508217159ed58b5b3e0daa500c2744d4
VaNomad <sjur.hassel@gmail.com>
2023-10-18 14:15:16 +0100

    Adds carousel to bookings card

--- src/components/cards/BookingsCard.jsx
+++ src/components/cards/BookingsCard.jsx
@@ -1,5 +1,5 @@
 // import { MyBookings } from "../../api/MyBookings";
-// import { VenuesCarousel } from "../carousel/VenuesCarousel";
+import { VenuesCarousel } from "../carousel/VenuesCarousel";
 import { BtnFull } from "../ui/buttons/BtnFull";
 
 export const BookingsCard = ({ data }) => {
@@ -22,15 +22,8 @@ export const BookingsCard = ({ data }) => {
       <div className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
         {data ? (
           <div className="card container mx-auto p-3 ">
-            <div>
-              <img
-                src={media}
-                alt="User Avatar"
-                className="w-[20px] h-[20px] rounded-full object-cover"
-              />
-            </div>
+            <VenuesCarousel media={media} name={name} />
             <h2>{name}</h2>
-            <p>{venueId}</p>
             <div>
               <div>
                 <p>Max number of guests: {maxGuests}</p>
@@ -59,8 +52,8 @@ export const BookingsCard = ({ data }) => {
                   <p>Breakfast: {breakfast}</p>
                   <p>Parking: {parking}</p>
                   <p>Pets allowed: {pets}</p>
-                  <p>Wifi: { wifi }</p>
-                  <p>Price pr night: { price }</p>
+                  <p>Wifi: {wifi}</p>
+                  <p>Price pr night: {price}</p>
                   <p>Rating {rating} </p>
                 </div>
               </div>
