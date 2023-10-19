6fa8c00e6b2153c401b03c88849c4e70d0d48513
VaNomad <sjur.hassel@gmail.com>
2023-10-19 21:05:03 +0100

    Adds conditional render to footer in layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,20 @@
 import { Header } from "./header/Header"
 import { Footer } from "./footer/Footer"
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+
 
 export const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {location.pathname === "/" || location.pathname === "/home" ? null : (
+        <Footer />
+      )}
     </div>
   );
 }
