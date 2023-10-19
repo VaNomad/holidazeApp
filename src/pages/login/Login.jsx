965abbd93f8fc963989726d51fbdd0999211d9f3
VaNomad <sjur.hassel@gmail.com>
2023-10-19 22:10:49 +0100

    styling updates

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,8 +2,8 @@ import { LoginForm } from "../../components/forms/LoginForm";
 
 export const Login = () => {
   return (
-    <div className="pt-24">
-      <LoginForm className="" />
+    <div>
+      <LoginForm />
     </div>
   );
 };
