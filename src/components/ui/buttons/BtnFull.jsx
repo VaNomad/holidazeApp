8a01d612397e90c74d7faa75ea88674d844af77a
VaNomad <sjur.hassel@gmail.com>
2023-10-20 20:32:05 +0100

    updated button template

--- src/components/ui/buttons/BtnFull.jsx
+++ src/components/ui/buttons/BtnFull.jsx
@@ -1,10 +1,11 @@
-import { TbArrowRight } from "react-icons/tb";
+import { BsSearchHeart } from "react-icons/bs";
 
 export const BtnFull = () => {
   return (
-    <button className="flex items-center justify-between rounded-full bg-holipink py-2 px-6 my-4 text-black tracking-widest font-dm text-[18px]">
-      Check Availability
-      <TbArrowRight className="ms-4" />
+    <button
+      className="flex items-center bg-none border-2 border-holiblue bg-holiblue font-semibold rounded-full p-2  text-black font-alli text-xl hover:bg-black hover:text-holiblue hover:scale-105 transition-all duration-300"
+    >
+      <BsSearchHeart size={18} />
     </button>
   );
 };
