777c6e13b7a83fb2c30530e77bc4fdbd301487d0
VaNomad <sjur.hassel@gmail.com>
2023-10-20 16:05:00 +0100

    Adds search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,34 @@
-export const Search = () => {
+import { useState } from "react";
+import { BsSearchHeart } from "react-icons/bs";
+
+export const SearchBar = ({ onSearch }) => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    onSearch(e.target.value);
+    setSearch(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>SearchComponent</h1>
+    <div className="my-2">
+      <form>
+        <div className="flex items-center justify-center gap-2">
+          <input
+            className="px-4 py-1 rounded-full bg-zinc-700 border-2 border-zinc-600 font-dm tracking-widest"
+            type="text"
+            name="text"
+            placeholder="Search for venues..."
+            value={search}
+            onChange={handleSearch}
+          />
+          <button
+            onClick={handleSearch}
+            className="flex items-center bg-none border-2 border-holiblue bg-holiblue font-semibold rounded-full p-2  text-black font-alli text-xl hover:bg-black hover:text-holiblue hover:scale-105 transition-all duration-300"
+          >
+            <BsSearchHeart size={18} />
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
