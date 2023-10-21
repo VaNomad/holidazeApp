1ff69089220413f748a537416fd22f465e652708
VaNomad <sjur.hassel@gmail.com>
2023-10-21 22:40:25 +0100

    searchbar updates

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -11,15 +11,17 @@ export const SearchBar = ({ onSearch }) => {
 
   return (
     <div className="mb-14">
-      <form>
-        <input
-          className="px-4 py-1 rounded-full bg-zinc-100 text-black font-alli font-semibold tracking-widest"
-          type="text"
-          name="text"
-          placeholder="Search..."
-          value={search}
-          onChange={handleSearch}
-        />
+      <form className="max-w-screen">
+        <div className="">
+          <input
+            className="max-w-full px-4 py-1 rounded-full bg-zinc-100 text-black font-alli font-semibold tracking-widest"
+            type="text"
+            name="text"
+            placeholder="Search..."
+            value={search}
+            onChange={handleSearch}
+          />
+        </div>
       </form>
     </div>
   );
