47736cce377c5fd97643b2a4bd6996b6cfad02b8
VaNomad <sjur.hassel@gmail.com>
2023-10-21 14:13:33 +0100

    MyVenuesCard styling

--- src/components/cards/MyVenuesCard.jsx
+++ src/components/cards/MyVenuesCard.jsx
@@ -2,25 +2,20 @@
 import { VenuesCarousel } from "../carousel/VenuesCarousel";
 import { UpdateVenueForm } from "../forms/UpdateVenueForm";
 import { ConfirmDelete } from "../forms/DeleteVenueForm";
-import { BsPeople, BsWifi } from "react-icons/bs";
-import { PiBowlFood } from "react-icons/pi";
-import { GiHollowCat } from "react-icons/gi";
+import { BsPeople, BsWifi, BsMailbox } from "react-icons/bs";
+import { PiBowlFood, PiHashStraightBold } from "react-icons/pi";
 import { CiParking1 } from "react-icons/ci";
-import { GiCheckMark, GiCrossMark } from "react-icons/gi";
-import { IoPricetagsOutline} from "react-icons/io5";
-
-// import { BtnFull } from "../ui/buttons/BtnFull";
-// import { useUser } from "../../context/UserContext";
-// import { CreateVenueForm } from "../forms/CreateVenueForm";
-// import { Link } from "react-router-dom";
-// import {TbArrowRight} from "react-icons/tb"
+import { GiCheckMark, GiCrossMark, GiWorld, GiHollowCat } from "react-icons/gi";
+import { IoPricetagsOutline, IoHome } from "react-icons/io5";
+import { FaCity, FaMapPin } from "react-icons/fa";
+import { AiFillStar } from "react-icons/ai";
+import { GoHistory } from "react-icons/go";
 
 export const MyVenuesCard = ({ data }) => {
-  // const { user, isAuthenticated } = useUser();
   const {
     created: venueCreated,
     id: venueId,
-    location: { address, city, continent, country, zip },
+    location: { address, city, country, zip },
     maxGuests,
     media,
     meta: { breakfast, parking, pets, wifi },
@@ -29,14 +24,17 @@ export const MyVenuesCard = ({ data }) => {
     rating,
   } = data;
 
+  const options = { day: "2-digit", month: "short", year: "numeric" };
+  const posted = new Date(venueCreated).toLocaleDateString(undefined, options);
+
   return (
     <div className="rounded-2xl p-2">
-      <div className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+      <div className="card container mx-auto p-2 bg-zinc-900 rounded-3xl text-xs">
         {data ? (
-          <div className="card container mx-auto p-3">
-            <h3 className="font-alli text-3xl">{name}</h3>
-            <div className="flex flex-col text-sm font-thin font-dm pb-2">
-              <div className="py-2">
+          <div className="card container mx-auto px-4">
+            <h3 className="font-alli text-3xl pb-2">{name}</h3>
+            <div className="flex flex-col text-sm font-thin font-dm">
+              <div>
                 <VenuesCarousel media={media} name={name} />
               </div>
               <div className="flex items-center gap-4">
@@ -48,17 +46,15 @@ export const MyVenuesCard = ({ data }) => {
                 </div>
               </div>
               <div>
-                <h2>
-                  <span className="font-semibold tracking-widest text-xs me-2">
-                    Venue Id:{" "}
-                  </span>{" "}
-                  {venueId}
-                </h2>
+                <div className="flex items-center gap-5 bg-zinc-700 rounded-full px-3 py-1 whitespace-nowrap">
+                  <PiHashStraightBold size={18} className="text-holipink" />
+                  <p className=" overflow-scroll">{venueId}</p>
+                </div>
               </div>
             </div>
             <div>
-              <div className="font-semibold text-xs">
-                <div className="font-dm tracking-wider px-3 mb-4">
+              <div className="font-dm text-xs py-2">
+                {/* <div className="font-dm tracking-wider mb-4">
                   <h3 className="font-alli font-thin text-3xl text-zinc-400 py-2">
                     Location Data:
                   </h3>
@@ -94,12 +90,200 @@ export const MyVenuesCard = ({ data }) => {
                       <p>{zip}</p>
                     </div>
                   </div>
+                </div> */}
+
+                {/* Location Details */}
+                <div className="card container mx-auto px-3 pb-3 my-2 bg-zinc-800 rounded-3xl text-xs">
+                  {/* <VenuesCarousel media={media} name={name} /> */}
+                  <h3 className="font-alli font-thin text-3xl text-zinc-400 pb-2">
+                    Location Details:
+                  </h3>
+                  <div className="flex flex-wrap gap-1">
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <GoHistory size={18} className="text-holiblue" />
+                      <p className="">{posted}</p>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <IoHome size={18} className="text-holiblue" />
+                      <p className="">{name}</p>
+                    </div>
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <BsPeople size={18} className="text-holiblue" />
+                      <p>{maxGuests}</p>
+                    </div> */}
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <GiWorld size={18} className="text-holiblue" />
+                      <p>{country}</p>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <FaCity size={18} className="text-holiblue" />
+                      <p>{city}</p>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <FaMapPin size={18} className="text-holiblue" />
+                      <p>
+                        {address}, {zip}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <BsMailbox size={18} className="text-holiblue" />
+                      <p>{zip}</p>
+                    </div>
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <PiBowlFood size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {breakfast ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <BsWifi size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {wifi ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <GiHollowCat size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {pets ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <CiParking1 size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {parking ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <IoPricetagsOutline size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {price ? <div>{price}</div> : <div>0</div>}
+                      </div>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <AiFillStar size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {rating ? (
+                          <div>{rating} / 5</div>
+                        ) : (
+                          <div className="text-[8px] font-dm uppercase">
+                            No rating
+                          </div>
+                        )}
+                      </div>
+                    </div> */}
+                  </div>
                 </div>
-                <ul className="flex flex-col text-white uppercase text-xs font-thin divide-y-[1px] divide-zinc-600 border border-zinc-600 rounded-xl p-2 gap-1">
+
+                {/* Venue Features */}
+                <div className="card container mx-auto px-3 pb-3 mt-2 bg-zinc-800 rounded-3xl text-xs">
+                  {/* <VenuesCarousel media={media} name={name} /> */}
+                  <h3 className="font-alli font-thin text-3xl text-zinc-400 pb-2">
+                    Venue Features:
+                  </h3>
+                  <div className="flex flex-wrap gap-1">
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <IoHome size={18} className="text-holiblue" />
+                      <p className="">{name}</p>
+                    </div> */}
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <BsPeople size={18} className="text-holiblue" />
+                      <p>{maxGuests}</p>
+                    </div>
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <GiWorld size={18} className="text-holiblue" />
+                      <p>{country}</p>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <FaCity size={18} className="text-holiblue" />
+                      <p>{city}</p>
+                    </div> */}
+                    {/* <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <FaMapPin size={18} className="text-holiblue" />
+                      <p>{address}</p>
+                    </div> */}
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <PiBowlFood size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {breakfast ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <BsWifi size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {wifi ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <GiHollowCat size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {pets ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <CiParking1 size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {parking ? (
+                          <GiCheckMark color="#70C376" />
+                        ) : (
+                          <GiCrossMark color="#C37070" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <IoPricetagsOutline size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {price ? <div>{price}</div> : <div>0</div>}
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5 bg-blackish rounded-full px-3 py-1 whitespace-nowrap overflow-x-scroll">
+                      <AiFillStar size={18} className="text-holiblue" />
+                      <div className="flex justify-end items-center">
+                        {rating ? (
+                          <div>{rating} / 5</div>
+                        ) : (
+                          <div className="text-[8px] font-dm uppercase">
+                            No rating
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* <ul className="flex flex-col text-white uppercase text-xs font-thin divide-y-[1px] divide-zinc-600 border border-zinc-600 rounded-xl p-2 gap-1">
                   <li className="grid grid-cols-3">
                     <BsPeople size={15} />
                     <h2>max guests</h2>
-                    <div className="flex justify-end items-center">{maxGuests}</div>
+                    <div className="flex justify-end items-center">
+                      {maxGuests}
+                    </div>
                   </li>
                   <li className="grid grid-cols-3">
                     <PiBowlFood size={22} />
@@ -149,7 +333,7 @@ export const MyVenuesCard = ({ data }) => {
                     <IoPricetagsOutline size={22} />
                     <h2 className="flex items-center">Price per night</h2>
                     <div className="flex justify-end items-center">
-                      {price ? (<div>{price}</div>) : (<div>0</div>)}
+                      {price ? <div>{price}</div> : <div>0</div>}
                     </div>
                   </li>
                   <li className="grid grid-cols-3">
@@ -164,7 +348,7 @@ export const MyVenuesCard = ({ data }) => {
                       )}
                     </div>
                   </li>
-                </ul>
+                </ul> */}
               </div>
             </div>
           </div>
