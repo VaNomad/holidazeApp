34b83165f3722e8210d85d894e6930fb98c64e8f
VaNomad <sjur.hassel@gmail.com>
2023-10-21 11:45:28 +0100

    testing new api call to replace ApiId.jsx

--- src/api/AvailableDates.jsx
+++ src/api/AvailableDates.jsx
@@ -1,48 +1,66 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { API_BASE_URL } from "./endpoints";
-import { } from "react-datepicker"
 import { ErrorDisplay } from "../components/ui/messages/ErrorDisplay";
 import { Loader } from "../components/ui/loader/Loader";
+import { BookingsCard } from "../components/cards/BookingsCard";
 
-export const ApiId = () => {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+
+export function AvailableDates() {
+  const [bookingData, setBookingData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
   const [errorDisplay, setErrorDisplay] = useState(null);
   let { id } = useParams();
   console.log(id);
 
   useEffect(() => {
-    async function fetchData(url) {
+    const fetchData = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+      console.log(accessToken);
+      const userParsed = JSON.parse(localStorage.getItem("user"));
+      const user = userParsed.name;
+      console.log(user);
+      const isUrl = `${API_BASE_URL}/bookings`;
+
       try {
         setIsLoading(true);
         setHasError(false);
+        const response = await fetch(isUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
 
-        const response = await fetch(url);
-        console.log(response);
-
-        if (!response) {
-          throw new Error("Could not return data from the API");
+        if (!response.ok) {
+          throw new Error(
+            `Could not return data from the API: Status: ${response.status}`
+          );
         }
+
+        console.log("RESPONSE FROM MYBOOKINGS", response);
 
         const data = await response.json();
         console.log(data);
+        // console.log("bookingData:", bookingData);
 
-        setData(data);
+        setBookingData(data);
+        setIsLoading(false);
       } catch (error) {
-        console.log("Api Catch Error", error);
+        console.error("Error:", error);
         setHasError(true);
         setErrorDisplay(error.message);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
 
-    fetchData(`${API_BASE_URL}/venues/${id}?_owner=true&booking=true`);
-  }, [id]);
+    fetchData();
+  }, []);
 
-  if (isLoading || !data) {
+  if (isLoading || !bookingData) {
     return (
       <div>
         <Loader />
@@ -53,14 +71,23 @@ export const ApiId = () => {
   if (hasError) {
     return (
       <div>
-        <ErrorDisplay message={errorDisplay.errorMessage} />
+        <ErrorDisplay message={errorDisplay} />
       </div>
     );
   }
 
+  if (bookingData.length === 0) {
+    return <div>No bookings available.</div>;
+  }
+
+  console.log("LAST LOG BEFORE MOUNT:", bookingData);
+
   return (
     <div>
-      {/* <Calendar key={data.id} data={data} /> */}
+      {bookingData.bookings.map((data) => {
+        console.log(data);
+        return <BookingsCard data={data} key={data.id} />;
+      })}
     </div>
   );
-};
+}
