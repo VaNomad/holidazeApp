8fa6430193279339a8b80d470acf29708972652b
VaNomad <sjur.hassel@gmail.com>
2023-10-22 16:52:05 +0100

    bg image update on login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import { LoginForm } from "../../components/forms/LoginForm";
 
 export const Login = () => {
   return (
-    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-fixed h-screen flex items-center">
+    <div className="h-screen flex items-center">
       <LoginForm />
     </div>
   );
