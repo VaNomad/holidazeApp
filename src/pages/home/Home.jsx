ed5289e3355cb519d76dc1c954572204ddc62299
Sjur <sjur.hassel@gmail.com>
2023-10-22 04:17:18 +0100

    Testing to see if tailwind bg imag is not working

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,18 +9,14 @@ export const Home = () => {
   // };
 
   return (
-    <div id="top" className="bg-cover bg-hero-pattern bg-fixed md:bg-center h-[100vh]">
-      <Hero />
-      <VenueList />
-      <SearchBtn />
-      {/* <div className="fixed bottom-4 right-4 z-40">
-        <button
-          onClick={scrollToTop}
-          className="border-[3px] border-black bg-holipink hover:bg-black text-black hover:text-holipink font-bold p-2 rounded-full hover:scale-105 transition-all duration-300"
-        >
-          <BsSearchHeart size={20} />
-        </button>
-      </div> */}
-    </div>
-  );
+  <div
+    id="top"
+    className="bg-hero-pattern bg-fixed md:bg-center"
+    style={{ backgroundSize: "cover", height: "100vh" }}
+  >
+    <Hero />
+    <VenueList />
+    <SearchBtn />
+  </div>
+);
 };
