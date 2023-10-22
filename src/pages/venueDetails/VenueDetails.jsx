35722ac94aa3e753f84de24fd0b3785ee2f5977a
VaNomad <sjur.hassel@gmail.com>
2023-10-22 22:34:03 +0100

    more style updates

--- src/pages/venueDetails/VenueDetails.jsx
+++ src/pages/venueDetails/VenueDetails.jsx
@@ -3,7 +3,7 @@ import { ApiId } from "../../api/ApiId";
 export const VenueDetails = () => {
   
   return (
-    <div className="bg-hero-pattern bg-fixed md:bg-center bg-cover">
+    <div>
       <ApiId />
     </div>
   );
